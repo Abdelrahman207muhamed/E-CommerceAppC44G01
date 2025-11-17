5cee54dc10901102af1b843c60d1e107c920b8ec
user <user@DESKTOP-0FTCJBS>
2025-11-17 02:09:19 +0200

    Session9 Doneee

--- Core/DomainLayer/Models/IdentityModule/ApplicationUser.cs
+++ Core/DomainLayer/Models/IdentityModule/ApplicationUser.cs
@@ -9,6 +9,7 @@ namespace DomainLayer.Models.IdentityModule
 {
     public class ApplicationUser:IdentityUser
     {
+
         public string DisplayName { get; set; } = string.Empty;
         public Address? Address { get; set; }   
     }
